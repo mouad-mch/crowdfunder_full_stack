dfc139a13be875781a4cd132b6a1657c8505ad63
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-05-22 11:01:44 +0100

    feat: refactor Dashboard component and add charts for project statistics

--- frontend/src/pages/Investors.jsx
+++ frontend/src/pages/Investors.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
-import { fetchProjectInvestors, fetchProjectById } from '../store/slices/projectSlice';
+// import { fetchProjectInvestors, fetchProjectById } from '../store/slices/projectSlice';
 
 const Investors = () => {
   const { id } = useParams();
